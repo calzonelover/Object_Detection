100d57e7026e2a7f952569b6670f4ba557bdd21c
calzonelover <patomporn.pay@gmail.com>
2018-12-18 01:08:40 +0700

    meed to optimize data feed

--- datasets/data.py
+++ datasets/data.py
@@ -1,3 +1,7 @@
+'''
+This may help: http://www.machinelearninguru.com/deep_learning/tensorflow/basics/tfrecord/tfrecord.html
+'''
+
 import tensorflow as tf
 from gluoncv import data, utils
 from matplotlib import pyplot as plt
@@ -12,6 +16,7 @@ print('Num of validation images:', len(val_dataset))
 
 train_image, train_label = train_dataset[52]
 print('Image size (height, width, RGB):', train_image.shape)
+print('Train label:', train_label)
 
 # tf_resize_img = tf.image.resize_images(
 #     tf.reshape(train_image.asnumpy(), [1, train_image.shape[0], train_image.shape[1], train_image.shape[2]]),
@@ -22,9 +27,30 @@ print('Image size (height, width, RGB):', train_image.shape)
 # cv2.imwrite('out_img.jpg', a)
 
 resized_img = cv2.resize(train_image.asnumpy(), (448, 448))
+resized_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
+train_image = cv2.cvtColor(train_image.asnumpy(), cv2.COLOR_BGR2RGB)
 print(resized_img.shape)
 cv2.imwrite('out_img.jpg', resized_img)
-cv2.imwrite('ori.img.jpg', train_image.asnumpy())
+cv2.imwrite('ori.img.jpg', train_image)
+
+
+# def load_data():
+#       x_train = np.arrays()
+#       y_train = []
+#       x_test = []
+#       y_test = []
+#       for i in range(len(train_dataset)):
+#             train_image, train_label = train_dataset[i]
+#             x_train.append(train_image.asnumpy())
+#             y_train.append(train_label)
+#       for i in range(len(val_dataset)):
+#             test_image, test_label = val_dataset[i]
+#             x_test.append(test_image.asnumpy())
+#             y_test.append(test_label)
+#       print(x_train.shape, y_train.shape)
+#       return (x_train, y_train), (x_test, y_test)
+
+# _1, _2, _3, _4 = load_data()
 
 '''
 from gluoncv import data, utils
